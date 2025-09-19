5357b1d68538f5cb2695cb1086e1e8411ca49ddb
Edwards Presume <edwardspresume@gmail.com>
2025-09-19 16:08:04 -0400

    Refactor main page tab navigation to improve organization; update tab values and integrate new icons from lucide-svelte for Timer, Settings, and Progress tabs. Adjust layout for better responsiveness.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,32 +2,38 @@
 	import FocusTimer from '$lib/components/FocusTimer.svelte';
 	import SettingsPanel from '$lib/components/SettingsPanel.svelte';
 	import * as Tabs from '$lib/components/ui/tabs/index.js';
-	import { Timer } from 'lucide-svelte';
+	import { ChartColumnIncreasing, Settings, Timer } from 'lucide-svelte';
 </script>
 
-<main class="p-6">
+<main class="p-6 h-dvh">
 	<h1 class="mb-8 text-center text-4xl font-bold">Focalite</h1>
 
-	<Tabs.Root value="focusTimer" class="w-[400px]">
-		<Tabs.Content value="focusTimer">
+	<Tabs.Root value="timer" class="w-[400px]">
+		<Tabs.Content value="timer">
 			<FocusTimer />
 		</Tabs.Content>
 
-		<Tabs.Content value="settingsPanel">
+		<Tabs.Content value="settings">
 			<SettingsPanel />
 		</Tabs.Content>
 
-		<Tabs.Content value="Progress">
+		<Tabs.Content value="progress">
 			<SettingsPanel />
 		</Tabs.Content>
 
-		<Tabs.List>
-			<Tabs.Trigger value="focusTimer">
+		<Tabs.List class=''>
+			<Tabs.Trigger value="timer">
 				<Timer />
 				<span>Timer</span>
 			</Tabs.Trigger>
-			<Tabs.Trigger value="settingsPanel">Settings</Tabs.Trigger>
-			<Tabs.Trigger value="Progress">Progress</Tabs.Trigger>
+			<Tabs.Trigger value="settings">
+				<Settings />
+				<span>Settings</span>
+			</Tabs.Trigger>
+			<Tabs.Trigger value="progress">
+				<ChartColumnIncreasing />
+				<span>Progress</span>
+			</Tabs.Trigger>
 		</Tabs.List>
 	</Tabs.Root>
 </main>
